0025378a07ceff1f16869e55951d28994eeaceab
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-09-28 15:08:21 +0000

    add properties package

---
+++ trunk/seleniumHome/src/test/java/org/selenium/properties/ObjectPropertiesXPATH.java
@@ -0,0 +1,11 @@
+package org.selenium.properties;
+
+public class ObjectPropertiesXPATH {
+	
+	
+	public final static String wl_desc_project = "//li/a[@title='Selenium Projects']";
+	public final static String wl_desc_download = "";
+	public final static String wl_desc_documentation = "";
+	
+	
+}
