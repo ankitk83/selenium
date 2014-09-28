0528386833aafba3665d0fa816930690eaf7716d
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-09-28 13:46:46 +0000

    initial check in

---
+++ seleniumHome/src/test/java/org/selenium/home/TestHome.java
@@ -0,0 +1,11 @@
+package org.selenium.home;
+
+import org.testng.annotations.Test;
+
+public class TestHome {
+  @Test
+  public void HomeCall() {
+	  
+	  System.out.println("H1. The test Was successfull...");
+  }
+}
