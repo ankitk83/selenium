6bf570a168177c32403a580621488baea856c48f
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-10-02 00:26:17 +0000

    initial check in

---
+++ trunk/seleniumHome/src/test/java/com/bugzilla/test/auth/BasicLoginLogout.java
@@ -0,0 +1,29 @@
+package com.bugzilla.test.auth;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.bugzilla.gra.login.GRA_Login;
+import com.bugzilla.gra.login.GRA_Logout;
+
+public class BasicLoginLogout {
+	
+	WebDriver local_webDriver = new FirefoxDriver();
+
+	GRA_Login login = PageFactory.initElements(local_webDriver, GRA_Login.class);
+	GRA_Logout logout = PageFactory.initElements(local_webDriver, GRA_Logout.class);
+	
+	
+	
+	@Test
+	public void BasicCheck(){
+		local_webDriver.get("http://10.0.0.20/bugzilla");
+		login.fn_plainLogin();
+		logout.fn_Logout();
+		
+	}
+    
+
+}
