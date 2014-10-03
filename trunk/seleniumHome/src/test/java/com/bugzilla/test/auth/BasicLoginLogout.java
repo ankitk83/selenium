8deb7f175c4642bffe68cfa8ec46bd1548bb623b
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-10-03 02:18:45 +0000

    added @AfterTest

--- trunk/seleniumHome/src/test/java/com/bugzilla/test/auth/BasicLoginLogout.java
+++ trunk/seleniumHome/src/test/java/com/bugzilla/test/auth/BasicLoginLogout.java
@@ -3,6 +3,8 @@ package com.bugzilla.test.auth;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.bugzilla.gra.login.GRA_Login;
@@ -10,11 +12,10 @@ import com.bugzilla.gra.login.GRA_Logout;
 
 public class BasicLoginLogout {
 	
-	WebDriver local_webDriver = new FirefoxDriver();
-
-	GRA_Login login = PageFactory.initElements(local_webDriver, GRA_Login.class);
-	GRA_Logout logout = PageFactory.initElements(local_webDriver, GRA_Logout.class);
 	
+		WebDriver local_webDriver = new FirefoxDriver();
+		GRA_Login login = PageFactory.initElements(local_webDriver, GRA_Login.class);
+		GRA_Logout logout = PageFactory.initElements(local_webDriver, GRA_Logout.class);
 	
 	
 	@Test
@@ -22,8 +23,12 @@ public class BasicLoginLogout {
 		local_webDriver.get("http://10.0.0.20/bugzilla");
 		login.fn_plainLogin();
 		logout.fn_Logout();
-		
 	}
     
+	@AfterTest
+	public void closeAll(){
+		local_webDriver.close();
+		local_webDriver.quit();
+	}
 
 }
