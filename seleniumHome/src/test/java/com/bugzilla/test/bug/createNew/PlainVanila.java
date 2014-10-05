c29f1b9b5ae998b49dabec5b2bd63f4275a367fa
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-10-05 21:55:00 +0000

    clean up done. added HashMap object to pass data.

--- seleniumHome/src/test/java/com/bugzilla/test/bug/createNew/PlainVanila.java
+++ seleniumHome/src/test/java/com/bugzilla/test/bug/createNew/PlainVanila.java
@@ -1,7 +1,17 @@
 package com.bugzilla.test.bug.createNew;
 
+
+/*
+ *
+ * 
+ * 
+ * 
+ */
+
+
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -10,35 +20,66 @@ import org.testng.annotations.Test;
 import com.bugzilla.gra.login.GRA_Login;
 import com.bugzilla.gra.login.GRA_Logout;
 import com.bugzilla.gra.nav.TopNav.GRA_TopNav;
-import com.bugzilla.support.groups.BeforeTestClass;
+import com.bugzilla.support.groups.PreTestLoader;
+import com.bugzilla.support.groups.StaticURLs;
 
-@Test
-public class PlainVanila extends BeforeTestClass{
+public class PlainVanila extends PreTestLoader{
+	
+	/*
+	 * Create objects for 
+	 */
+	WebDriver oWebDriver;
+	GRA_Login oLogin;
+	GRA_TopNav oTopNav;
+	GRA_Logout oLogout; 
+	
 	
 	@BeforeTest
 	public void callSuperClass(){
-		super.BeforeTestClass1();
-	}
-	WebDriver local_webDriver = new FirefoxDriver();
+		/*
+		 * load function from superClass which does following functions:
+		 * 	1. Property Values.
+		 * 	2. Sets Browser object.
+		 * 	3. TBD 
+		 */
+		BeforeTestClass1();
+		
+		/*
+		 * check loading of property values from subClass
+		 */
+		System.out.println("called in subclass" + Prop.getProperty("x1"));
 
-	GRA_Login login = PageFactory.initElements(local_webDriver, GRA_Login.class);
-	GRA_TopNav topNav = PageFactory.initElements(local_webDriver, GRA_TopNav.class);
-	GRA_Logout logout = PageFactory.initElements(local_webDriver, GRA_Logout.class);
+		//Get webDriver object based on Browser value set in TestNG.xml; 
+		oWebDriver = getBrowser();
+		
+		//Initiate objects for all GRAs.
+		oLogin = PageFactory.initElements(oWebDriver, GRA_Login.class);
+		oTopNav = PageFactory.initElements(oWebDriver, GRA_TopNav.class);
+		oLogout = PageFactory.initElements(oWebDriver, GRA_Logout.class);
+	}
 	
-	
+
 	@Test
-	public void BasicCheck(){
-		local_webDriver.get("http://10.0.0.20/bugzilla");
-		login.fn_plainLogin();
-		topNav.fn_navigateToNewBug();
-		logout.fn_Logout();
+	public void BasicCheck() throws IOException {
+
+		//Open Target Page.
+		oWebDriver.get(StaticURLs.bugZillaHome);
+		
+		// Login
+		oLogin.fn_plainLogin(m1);
+		
+		//Click on New link in top header
+		oTopNav.fn_navigateToNewBug();
+		
+		//Logout
+		oLogout.fn_Logout();
 		
 	}
     
 	@AfterTest
 	public void closeAll(){
-		local_webDriver.close();
-		local_webDriver.quit();
+		oWebDriver.close();
+		oWebDriver.quit();
 	}
 
 }
