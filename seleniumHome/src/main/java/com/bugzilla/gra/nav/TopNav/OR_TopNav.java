555d16abba2da0027394ebe15d124784e00797c6
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-10-05 21:46:22 +0000

    initial check in

---
+++ seleniumHome/src/main/java/com/bugzilla/gra/nav/TopNav/OR_TopNav.java
@@ -0,0 +1,12 @@
+package com.bugzilla.gra.nav.TopNav;
+
+public class OR_TopNav {
+
+
+	final public static String val_wl_newBug 	= "//li/a[text()='New']";
+//	final public static String val_we_username	= "//form/input[@id='Bugzilla_login_top']";
+//	final public static String val_we_password	= "//form/input[@id='Bugzilla_password_top']";
+//	final public static String val_wb_login		= "//form/input[@id='log_in_top']";
+//	final public static String val_wl_logout	= "//li/a[contains(@href,'logout')]";
+
+}
