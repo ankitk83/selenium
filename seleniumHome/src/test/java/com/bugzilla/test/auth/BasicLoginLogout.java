b56b80ca9e90c2b6623cc6c45bea3d3d9ae040ff
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-10-22 02:30:44 +0000

    1. added dynamic Pagefactory initiation from PreTestLoader. 2. added Listener for start and end of test

--- seleniumHome/src/test/java/com/bugzilla/test/auth/BasicLoginLogout.java
+++ seleniumHome/src/test/java/com/bugzilla/test/auth/BasicLoginLogout.java
@@ -5,38 +5,26 @@ import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.bugzilla.gra.login.GRA_Login;
 import com.bugzilla.gra.login.GRA_Logout;
 import com.bugzilla.support.groups.PreTestLoader;
+import com.bugzilla.support.groups.StaticURLs;
 
-@Test
+
 public class BasicLoginLogout extends PreTestLoader{
-	WebDriver local_webDriver ;
-	GRA_Login login;
-	GRA_Logout logout; 
-
-	@BeforeTest
-	public void callSuperClass(){
-
-		 local_webDriver = getBrowser();
-		 login = new GRA_Login(local_webDriver);
-		 logout = new GRA_Logout(local_webDriver);
-	}
 	
-	public void BasicCheck() throws IOException{
-		local_webDriver.get("http://10.0.0.20/bugzilla");
-		login.fn_plainLogin(m1);
-		logout.fn_Logout();
+	@Test
+	public void BasicLoginLogout() throws IOException{
+		BrowserDriver.get(StaticURLs.bugZillaHome);
+		graLogin.fn_plainLogin(m1);
+		graLogout.fn_Logout();
 	}
     
-	@AfterTest
-	public void closeAll(){
-		local_webDriver.close();
-		local_webDriver.quit();
-	}
 
 }
--- seleniumHome/src/test/java/com/bugzilla/test/bug/createNew/CreateNewBug.java
+++ seleniumHome/src/test/java/com/bugzilla/test/bug/createNew/CreateNewBug.java
@@ -1,9 +1,12 @@
 package com.bugzilla.test.bug.createNew;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -12,44 +15,22 @@ import com.bugzilla.gra.login.GRA_Login;
 import com.bugzilla.gra.login.GRA_Logout;
 import com.bugzilla.gra.nav.TopNav.GRA_TopNav;
 import com.bugzilla.support.groups.PreTestLoader;
+import com.bugzilla.support.groups.StaticURLs;
+
 
 public class CreateNewBug extends PreTestLoader{
 	
 	
-	WebDriver oWebDriver;
 	
-
-	GRA_Login oLogin;
-	GRA_TopNav oTopNav;
-	GRA_CreateBug oCreateBug;
-	GRA_Logout oLogout;
-	
-	@BeforeTest
-	public void callSuperClass(){
-		
-		oWebDriver = getBrowser();
-		oLogin = new GRA_Login(oWebDriver);
-		oTopNav = new GRA_TopNav(oWebDriver);
-		oCreateBug = new GRA_CreateBug(oWebDriver);
-		oLogout = new GRA_Logout(oWebDriver);
-	}
-	
-	
-
 	@Test
-	public void createNewBug(){
-		oWebDriver.get("http://10.0.0.20/bugzilla");
-		oLogin.fn_plainLogin(m1);
-		oTopNav.fn_navigateToNewBug();
-		oCreateBug.fn_CreateBug();
-		oLogout.fn_Logout();
+	public void CreateNewBug() throws IOException{
+		BrowserDriver.get(StaticURLs.bugZillaHome);
+		graLogin.fn_plainLogin(m1);
+		graTopNav.fn_navigateToNewBug();
+		graCreateBug.fn_CreateBug();
+		graLogout.fn_Logout();
 	}
 	
-	@AfterTest
-	public void closeAll(){
-//		oWebDriver.close();
-//		oWebDriver.quit();
-	}
 
 
 }
