7c5d4d436f4be9e05f781abe82cb14ebc1a60386
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-10-25 03:27:20 +0000

    modified changes. loading for class objects based on calling method. loading superMethod before every class

--- seleniumHome/src/main/java/com/bugzilla/support/groups/PreTestLoader.java
+++ seleniumHome/src/main/java/com/bugzilla/support/groups/PreTestLoader.java
@@ -1,43 +1,31 @@
 package com.bugzilla.support.groups;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.HashMap;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import com.bugzilla.gra.createbug.GRA_CreateBug;
-import com.bugzilla.gra.login.*;
+import com.bugzilla.gra.login.GRA_Login;
+import com.bugzilla.gra.login.GRA_Logout;
 import com.bugzilla.gra.nav.TopNav.GRA_TopNav;
 //import com.bugzilla.gra.login.GRA_Logout;
-import com.google.common.base.CaseFormat;
 
-public class PreTestLoader {
+public class PreTestLoader extends GRALoader {
 
 	private String oEnv;
 	private String oBrows;
 	protected WebDriver BrowserDriver;
 	public static HashMap m1;
 	public WebDriver local_webDriver;
-	
-	
-	public GRA_Login graLogin;
-	public GRA_Logout graLogout;
-	public GRA_TopNav graTopNav;
-	public GRA_CreateBug graCreateBug;
 	private String methodName;
 	
 	
@@ -52,90 +40,88 @@ public class PreTestLoader {
 		
 	}
 	
-	@BeforeMethod
-	public void nameBefore(Method method)
-	{
-	    System.out.println("Test name: " + method.getName()); 
-	    methodName = method.getName();
-
-	}
-	
+//	@BeforeMethod
+//	public void nameBefore(Method method)
+//	{
+//	    System.out.println("Test name: " + method.getName()); 
+//	    methodName = method.getName();
+//
+//	}
+//	
 	
 	@BeforeMethod
 	@Parameters({"environment","browser"})
-	public void getParams(@Optional String env, String brows, Method method) throws IOException{
+	public void fn_getParams(@Optional String env, String brows, Method method) throws IOException{
 	    System.out.println("Test name: " + method.getName());
 	    methodName = method.getName();
-		
-
 		System.out.println("called 1");
 		this.oEnv = env;
 		System.out.println("called from beforetest:::" + brows);
 		this.oBrows = brows;
-		System.out.println("class Name:" + this.getClass().getSimpleName() + "...");
-		System.out.println("[][]" + this.oBrows.toString().toUpperCase().trim()+ "[][]");
-		System.out.println(Thread.currentThread().getStackTrace());
-		System.out.println("--------------------");
+//		System.out.println("class Name:" + this.getClass().getSimpleName() + "...");
+//		System.out.println("[][]" + this.oBrows.toString().toUpperCase().trim()+ "[][]");
+//		System.out.println(Thread.currentThread().getStackTrace());
+//		System.out.println("--------------------");
 		fn_setLoadBrowser();
 		fn_loadTestData();
-		fn_loadClasses(methodName);
-		
+//		fn_loadClasses(methodName);
+		fn_loadClasses(methodName,BrowserDriver);
 
 	}
 	
 	
-	@AfterMethod
-	public void closeAllItems(){
-		BrowserDriver.close();
-		BrowserDriver = null;
-		graLogin = null;
-		
-	}
-	
-	
-	private void fn_loadClasses(String MethodName){
-		System.out.println("entering loadClass method");
-		
-		switch (MethodName){
-		case "PlainVanila":
-			
+	/*
+	private void fn_loadClasses(String methodName2) {
+		switch(methodName){
+		case "BasicLoginLogout":
+			System.out.println("Null Check before" + graLogout==null);
+
 			graLogin = PageFactory.initElements(BrowserDriver, GRA_Login.class);
 			graLogout = PageFactory.initElements(BrowserDriver, GRA_Logout.class);
-			graTopNav = PageFactory.initElements(BrowserDriver, GRA_TopNav.class);
-			System.out.println("loaded" + MethodName);
+			System.out.println("loaded from GRALOADER:::::::" + methodName);
+			System.out.println("Null Check after" + graLogout==null);
+
+			break;
+			
+	case "PlainVanila":
+			System.out.println("Null Check before" + graLogout==null);
+		graLogin = PageFactory.initElements(BrowserDriver, GRA_Login.class);
+		graLogout = PageFactory.initElements(BrowserDriver, GRA_Logout.class);
+		graTopNav = PageFactory.initElements(BrowserDriver, GRA_TopNav.class);
+			System.out.println("loaded" + methodName);
+			System.out.println("Null Check after" + graLogout==null);
+	
 		break;
 		
 		case "CreateNewBug":
-			
+			System.out.println("Null Check before" + graLogout==null);
+
 			graLogin = PageFactory.initElements(BrowserDriver, GRA_Login.class);
 			graLogout = PageFactory.initElements(BrowserDriver, GRA_Logout.class);
 			graTopNav = PageFactory.initElements(BrowserDriver, GRA_TopNav.class);
 			graCreateBug = PageFactory.initElements(BrowserDriver, GRA_CreateBug.class);
-			System.out.println("loaded" + MethodName);
+			System.out.println("loaded" + methodName);
+			System.out.println("Null Check after" + graLogout==null);
+
 		break;
 		
-		case "BasicLoginLogout":
-			
-			graLogin = PageFactory.initElements(BrowserDriver, GRA_Login.class);
-			graLogout = PageFactory.initElements(BrowserDriver, GRA_Logout.class);
-			System.out.println("loaded" + MethodName);
-		break;
+
 		
 		case "PlainSearch":
 			
 			graLogin = PageFactory.initElements(BrowserDriver, GRA_Login.class);
 			graLogout = PageFactory.initElements(BrowserDriver, GRA_Logout.class);
 			graTopNav = PageFactory.initElements(BrowserDriver, GRA_TopNav.class);
-			System.out.println("loaded" + MethodName);
+			System.out.println("loaded" + methodName);
+			System.out.println("Null Check after" + graLogout==null);
+
 		break;
 		
 		
 		}
-		
+			
 	}
-	
-	
-	
+*/
 	private void fn_setLoadBrowser(){
 		switch (this.oBrows.toString().toUpperCase().trim()) {
 		
@@ -153,6 +139,18 @@ public class PreTestLoader {
 			break;
 		}
 	}
+	
+	
+	
+	@AfterMethod
+	public void closeAllItems(){
+		BrowserDriver.close();
+		BrowserDriver = null;
+		graLogin = null;
+		graLogout = null;
+		
+	}
+	
 	
 	
 	public WebDriver getBrowser(){
