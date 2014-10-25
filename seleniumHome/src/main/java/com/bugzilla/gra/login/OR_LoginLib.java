cabede8c7e3dc9ff2009b5d59805768c91a4cdbd
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-10-25 03:28:48 +0000

    modified definition of Logout link

--- seleniumHome/src/main/java/com/bugzilla/gra/login/OR_LoginLib.java
+++ seleniumHome/src/main/java/com/bugzilla/gra/login/OR_LoginLib.java
@@ -1,3 +1,4 @@
+
 package com.bugzilla.gra.login;
 
 import org.openqa.selenium.WebElement;
@@ -6,11 +7,10 @@ import org.openqa.selenium.support.FindBy;
 public class OR_LoginLib {
 
 
-	final public static String 	val_wl_login 	= "//li/a[@id='login_link_top']";
+	final public static String val_wl_login 	= "//li/a[@id='login_link_top']";
 	final public static String val_we_username	= "//form/input[@id='Bugzilla_login_top']";
 	final public static String val_we_password	= "//form/input[@id='Bugzilla_password_top']";
 	final public static String val_wb_login		= "//form/input[@id='log_in_top']";
-	final public static String val_wl_logout	= "//li/a[contains(@href,'logout')]";
-	
-	
+//	final public static String val_wl_logout	= "//li/a[contains(@href,'logout')]";
+	final public static String val_wl_logout	= "//div[@id='common_links']/ul/li/a[@href='index.cgi?logout=1']";
 }
