a1572b971296737d85c71b223306331cbb389c34
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-11-11 23:00:24 +0000

    change the super class to suite level base classes

--- seleniumHome/src/test/java/com/bugzilla/test/auth/BasicLoginLogout.java
+++ seleniumHome/src/test/java/com/bugzilla/test/auth/BasicLoginLogout.java
@@ -2,29 +2,20 @@ package com.bugzilla.test.auth;
 
 import java.io.IOException;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.bugzilla.global.values.StaticURLs;
-import com.bugzilla.gra.login.GRA_Login;
-import com.bugzilla.gra.login.GRA_Logout;
-import com.bugzilla.support.groups.PreTestLoader;
+import com.bugzilla.test.base.AuthenticationBase;
+import com.bugzilla.test.base.TestBase;
 
 
-public class BasicLoginLogout extends PreTestLoader{
+public class BasicLoginLogout extends AuthenticationBase{
 	
 	@Test
 	public void BasicLoginLogout() throws IOException{
 		BrowserDriver.get(StaticURLs.bugZillaHome);
-		ts.testLoginAction(m1);
-//		graLogin.fn_plainLogin(m1);
-		graLogout.fn_Logout();
+		graLoginLogout.fn_TopNavLogin(m1);
+		graLoginLogout.fn_Logout();
 	}
     
 
--- seleniumHome/src/test/java/com/bugzilla/test/bug/createNew/CreateNewBug.java
+++ seleniumHome/src/test/java/com/bugzilla/test/bug/createNew/CreateNewBug.java
@@ -2,34 +2,21 @@ package com.bugzilla.test.bug.createNew;
 
 import java.io.IOException;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.bugzilla.global.values.StaticURLs;
-import com.bugzilla.gra.createbug.GRA_CreateBug;
-import com.bugzilla.gra.login.GRA_Login;
-import com.bugzilla.gra.login.GRA_Logout;
-import com.bugzilla.gra.nav.TopNav.GRA_TopNav;
-import com.bugzilla.support.groups.PreTestLoader;
+import com.bugzilla.test.base.BugCreateBase;
 
 
-public class CreateNewBug extends PreTestLoader{
-	
-	
+public class CreateNewBug extends BugCreateBase{
 	
 	@Test
-	public void CreateNewBug() throws IOException{
-		BrowserDriver.get(StaticURLs.bugZillaHome);
-		ts.testLoginAction(m1);
-//		graLogin.fn_plainLogin(m1);
+	public void BZ_CreateBug_BasicBug() throws IOException{
+		eDriver.get(StaticURLs.bugZillaHome);
+		graLoginLogout.fn_TopNavLogin(m1);
 		graTopNav.fn_navigateToNewBug();
 		graCreateBug.fn_CreateBug();
-		graLogout.fn_Logout();
+		graLoginLogout.fn_Logout();
 	}
 	
 
