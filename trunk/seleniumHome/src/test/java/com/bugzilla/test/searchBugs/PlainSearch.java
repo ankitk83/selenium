51d6ae55e8a603807b280db584d0dd0e3f4d4c2e
ankit.kapoor83@gmail.com <ankit.kapoor83@gmail.com@df97ea12-ef1f-83b1-1bb5-dfb3bfde7dcf>
2014-11-11 23:00:24 +0000

    change the super class to suite level base classes

--- trunk/seleniumHome/src/test/java/com/bugzilla/test/searchBugs/PlainSearch.java
+++ trunk/seleniumHome/src/test/java/com/bugzilla/test/searchBugs/PlainSearch.java
@@ -2,36 +2,22 @@ package com.bugzilla.test.searchBugs;
 
 import java.io.IOException;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.bugzilla.global.values.StaticURLs;
-import com.bugzilla.gra.login.GRA_Login;
-import com.bugzilla.gra.login.GRA_Logout;
-import com.bugzilla.gra.nav.TopNav.GRA_TopNav;
-import com.bugzilla.support.groups.PreTestLoader;
-
-import org.testng.annotations.BeforeClass;
+import com.bugzilla.test.base.BugSearchBase;
 
 
-public class PlainSearch extends PreTestLoader{
+public class PlainSearch extends BugSearchBase{
 
 
 	@Test
 	public void PlainSearch() throws IOException{
 		BrowserDriver.get(StaticURLs.bugZillaHome);
-		ts.testLoginAction(m1);
-//		graLogin.fn_plainLogin(m1);
-//		graLogin.wel_wl_login.click();
-//		graLogin.page("username").set("ankit.kapoor");
-//		graLogin.we_username().set("ankit.kapoor");
+		graLoginLogout.fn_TopNavLogin(m1);
 		graTopNav.fn_searchBugs();
 		graSearchBug.selectSomething();
-		graLogout.fn_Logout();
+		graLoginLogout.fn_Logout();
 	}
     
 
