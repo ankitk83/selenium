2bc96dabf6b44d87b571f1e21c83814503d88394
ankit kapoor <ankit.kapoor83@gmail.com>
2015-04-04 13:54:24 -0400

    test commit

--- seleniumHome/src/test/java/com/bugzilla/test/searchBugs/PlainSearch.java
+++ seleniumHome/src/test/java/com/bugzilla/test/searchBugs/PlainSearch.java
@@ -4,20 +4,20 @@ import java.io.IOException;
 
 import org.testng.annotations.Test;
 
-import com.bugzilla.global.values.StaticURLs;
-import com.bugzilla.test.base.BugSearchBase;
+//import com.bugzilla.test.base.BugCreateBase;
+////import com.bugzilla.test.base.BugSearchBase;
+//
 
-
-public class PlainSearch extends BugSearchBase{
+public class PlainSearch{
 
 
 	@Test
 	public void PlainSearch() throws IOException{
-		BrowserDriver.get(StaticURLs.bugZillaHome);
-		graLoginLogout.fn_TopNavLogin(m1);
-		graTopNav.fn_searchBugs();
-		graSearchBug.selectSomething();
-		graLoginLogout.fn_Logout();
+//		BrowserDriver.get(StaticURLs.bugZillaHome);
+//		graLoginLogout.fn_TopNavLogin(m1);
+//		graTopNav.fn_searchBugs();
+//		graSearchBug.selectSomething();
+//		graLoginLogout.fn_Logout();
 	}
     
 
